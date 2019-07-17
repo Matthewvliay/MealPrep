5beb4d9ab4665845757bed72c20e28928b5eeacd
Matthewvliay <42940122+Matthewvliay@users.noreply.github.com>
2019-07-17 16:55:02 -0500

    Add files via upload
    -FIxed flicker when switching to the ingredient page for each recipe.
    -Redirect user to home page when an invalid id is in the search bar.

---
+++ edit.js
@@ -0,0 +1,71 @@
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { initializeEditPage, renderRecipes } from './views'
+import { updateRecipe, removeRecipe } from './recipes'
+import { createIngredient } from './ingredients'
+import { renderIngredients } from './edit-views'
+const recipeId = location.hash.substring(1)
+
+if (recipeId) {
+    initializeEditPage(recipeId)
+} else {
+    location.assign('/index.html')
+}
+
+
+const titleElement = document.querySelector('#recipe-title')
+const bodyElement = document.querySelector('#recipe-body')
+const removeElement = document.querySelector('#remove-recipe')
+const newIngredientElement = document.querySelector('#new-ingredient')
+
+
+
+if (titleElement) {
+    titleElement.addEventListener('input', (e) => {
+        updateRecipe(recipeId, {
+            title: e.target.value
+        })
+    })
+}
+if (bodyElement) {
+    bodyElement.addEventListener('input', (e) => {
+        updateRecipe(recipeId, {
+            body: e.target.value
+        })
+    })
+}
+
+if (removeElement) {
+    removeElement.addEventListener('click', (e) => {
+        removeRecipe(recipeId)
+        location.assign('/index.html')
+    })
+}
+
+
+
+
+if (newIngredientElement) {
+    newIngredientElement.addEventListener('submit', (e) => {
+        const text = e.target.elements.text.value.trim()
+        e.preventDefault()
+        if (text.length > 0) {
+            createIngredient(recipeId, text)
+            renderIngredients()
+            e.target.elements.text.value = ''
+        }
+    })
+    newIngredientElement.appendChild(removeElement)
+}
+
+
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'recipe') {
+        initializeEditPage(recipeId)
+    }
+})
+// // getRecipes().forEach((recipe) => )
+
+export { renderIngredients } 
---
+++ index.js
@@ -0,0 +1,25 @@
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { createRecipe } from './recipes'
+import { renderRecipes } from "./views";
+
+renderRecipes()
+document.querySelector('#create-recipe').addEventListener('click', (e) => {
+
+    const id = createRecipe()
+    location.assign(`/edit.html#${id}`)
+})
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    setFilters({
+        searchText: e.target.value
+    })
+    renderRecipes()
+})
+
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'recipes') {
+        renderRecipes()
+    }
+})
